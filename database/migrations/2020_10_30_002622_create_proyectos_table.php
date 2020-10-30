1498b9bc5cd9d48c673c96815dddc1424e4acae6
Agustin Aguilar <agustin.aguilar5540@alumnos.udg.mx>
2020-10-29 18:46:49 -0600

    migracion

---
+++ database/migrations/2020_10_30_002622_create_proyectos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombreProyecto', 128);
+            $table->string('nombrePersona', 64);
+            $table->string('correo', 64);           
+            $table->text('detalles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+}
